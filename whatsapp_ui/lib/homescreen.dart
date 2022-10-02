aa6acc2f7d38e79bbea81c1ccb38ce578ec8acec
Exalter104 <muhammadsherazkhan104@gmail.com>
2022-10-03 00:01:12 +0500

    App: whatsapp Full UI is ready

--- whatsapp_ui/lib/homescreen.dart
+++ whatsapp_ui/lib/homescreen.dart
@@ -16,26 +16,84 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: const Text("WhatsApp"),
           backgroundColor: Colors.teal,
-          actions: const [
-            Icon(Icons.search_outlined),
-            SizedBox(
+          actions: [
+            const Icon(Icons.search_outlined),
+            const SizedBox(
               width: 10,
+            ),
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert_outlined),
+              itemBuilder: (context) => const [
+                PopupMenuItem(value: "1", child: Text("New Group")),
+                PopupMenuItem(value: "2", child: Text("Search Number")),
+                PopupMenuItem(value: "3", child: Text("Log Out")),
+              ],
             )
           ],
           bottom: const TabBar(
             tabs: [
-              Icon(Icons.camera_alt_rounded),
-              Text("Messages"),
-              Text("Calls"),
-              Text("Status"),
+              Tab(
+                child: Icon(Icons.camera_alt_rounded),
+              ),
+              Tab(child: Text("Messages")),
+              Tab(child: Text("Calls")),
+              Tab(child: Text("Status")),
             ],
           ),
         ),
-        body: const TabBarView(children: [
-          Text("camera"),
-          Text("Messages"),
-          Text("Calls"),
-          Text("Status"),
+        body: TabBarView(children: [
+          const Text("camera"),
+          ListView.builder(
+              itemCount: 40,
+              itemBuilder: ((context, index) {
+//Messages
+                return const ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage("images/me.png"),
+                  ),
+                  title: Text("Home ❤️"),
+                  subtitle: Text(
+                    "zargya Za tasra Dera Meena Kom ",
+                  ),
+                  trailing: Text("5:01"),
+                );
+              })),
+//Calls
+          ListView.builder(
+              itemCount: 40,
+              itemBuilder: ((context, index) {
+                return const ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage("images/me.png"),
+                  ),
+                  title: Text("Home ❤️"),
+                  subtitle: Text(
+                    "📞 October 01,5:0 PM",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  trailing: Icon(Icons.missed_video_call),
+                );
+              })),
+//Status
+
+          ListView.builder(
+              itemCount: 40,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  leading: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.green, width: 3)),
+                    child: const CircleAvatar(
+                      backgroundImage: AssetImage("images/me.png"),
+                    ),
+                  ),
+                  title: const Text("Home ❤️"),
+                  subtitle: const Text(
+                    "5:0 PM",
+                  ),
+                );
+              })),
         ]),
       ),
     );
